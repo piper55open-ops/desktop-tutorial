2de84e0aaeabdc6fd349542a36bb6410b44e3e3a
piper55open-ops <piper55open@gmail.com>
2025-08-09 12:17:33 +1200

    Create game.py

---
+++ week2/game.py
@@ -0,0 +1,24 @@
+import random
+def game():
+    list = ["apple", "banana", "cherry", "date", "elderberry"]
+    a=random.choice(list)
+    b=len(a)
+    word = [' ' for i in range(b)]
+    print("let's guess the letter, you can enter a letter")
+    num=0
+    i=0
+    life=10
+    while num<b:
+        letter=input()[0]
+        for i in range(0,b):
+            if letter==a[i] and word[i]==' ':
+                num=num+1
+                word[i]=letter
+                print("correct,the word is ",word)
+                break
+            if i==b-1:
+                life=life-1
+                print("wrong letter, you only have",life," of life")
+            if life==0:
+                return print("you lose")
+game()
